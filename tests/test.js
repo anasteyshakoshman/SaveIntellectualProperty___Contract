d189928faf2c39475f81ff1c9e078137c96003f2
Анастасия Кошман <anastasiay.andreevnaa@gmail.com>
2022-06-14 00:50:56 +0300

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,10 @@ const { expect } = require("chai");
 
 describe("Save Intellectual Property contract", () => {
   const tokenUnicalIdentifier = 'unical_identifier_token';
-  const baseURI = "https://hardhat.org/test/"
+  const author = {
+    name: 'Van Gog',
+    description: 'Drawer, millioner, filantrop'
+  }
   
   let contract;
   let owner;
@@ -13,25 +16,38 @@ describe("Save Intellectual Property contract", () => {
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("SaveIntellectualProperty");
     const [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
     contract = await Token.deploy();
-    await contract.setBaseURI(baseURI)
+    await contract.setAuthorName(owner.address, author.name);
+    await contract.setAuthorDescription(owner.address, author.description);
   });
 
-  it("Should initialize contract", async () => {
-    expect(await contract.baseURI).to.equal(baseURI);
-  });
+  const ownerAddress = await owner.address;
+
   it("Should set the right owner contract", async () => {
-    expect(await contract.owner()).to.equal(await owner.address);
+    expect(await contract.owner()).to.equal(ownerAddress);
+    expect(await contract.getAuthorName(ownerAddress)).to.equal(author.name);
+    expect(await contract.getAuthorDescription(ownerAddress)).to.equal(author.description);
   });
+
   it("Should mint", async () => {
     expect(
-        await contract.registerNewToken(tokenUnicalIdentifier)
-    ).to.emit(contract, "Transfer").withArgs(ethers.constants.AddressZero, owner.address, 1);  
+        await contract.registerNewToken(tokenUnicalIdentifier, ownerAddress)
+    ).to.emit(contract, "Transfer").withArgs(ethers.constants.AddressZero, ownerAddress, 1);  
   });
+
   it("Should be right owner and author token", async () => {
-      expect(await contract.ownerOf(1).to.equal(owner.address));
-      expect(await contract.ownerOfIdentifier(tokenUnicalIdentifier).to.equal(owner.address));
-      expect(await contract.authorOfIdentifier(tokenUnicalIdentifier).to.equal(owner.address));
-      expect(await contract.authorOfTokenId(1).to.equal(owner.address));
+      expect(await contract.ownerOf(1).to.equal(ownerAddress));
+      expect(await contract.authorOfTokenId(1).to.equal(ownerAddress));
+
+      expect(await contract.ownerOfIdentifier(tokenUnicalIdentifier).to.equal(ownerAddress));
+      expect(await contract.authorOfIdentifier(tokenUnicalIdentifier).to.equal(ownerAddress));
+
+      expect(await contract.getTokensByOwner(ownerAddress).to.equal([1]));
+      expect(await contract.getTokensByAuthor(ownerAddress).to.equal([1]));
+  });
+  
+  it("Should be right relation between identifier and tokenId", async () => {
+      expect(await contract.getImageIdentifier(1).to.equal(tokenUnicalIdentifier));
   });
 });
